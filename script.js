fc5eb2528039cdc4c7c645a398d602674302de55
ChloeLiang <nliangxin@gmail.com>
2018-09-24 10:26:09 +0800

    Add morseDecode and caesarEncode

--- script.js
+++ script.js
@@ -1,17 +1,92 @@
+var alphabet = {
+  a: '.-',
+  b: '-...',
+  c: '-.-.',
+  d: '-..',
+  e: '.',
+  f: '..-.',
+  g: '--.',
+  h: '....',
+  i: '..',
+  j: '.---',
+  k: '-.-',
+  l: '.-..',
+  m: '--',
+  n: '-.',
+  o: '---',
+  p: '.--.',
+  q: '--.-',
+  r: '.-.',
+  s: '...',
+  t: '-',
+  u: '..-',
+  v: '...-',
+  w: '.--',
+  v: '...-',
+  x: '-..-',
+  y: '-.--',
+  z: '--..'
+};
+
+var alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+function decodeAlphabet(alphabet) {
+  var char;
+  var code;
+  var alphabetDecode = {};
+
+  for (char in alphabet) {
+    code = alphabet[char];
+    alphabetDecode[code] = char;
+  }
+
+  return alphabetDecode;
+}
 
 function morseDecode(input) {
-  // Do your stuff here
+  var alphabetDecode = decodeAlphabet(alphabet);
+  var morse = input.split(' ');
+  var decodedMsg = [];
+  var i;
+  var code;
 
-  return "Your result";
+  for (i = 0; i < morse.length; i++) {
+    code = morse[i];
+
+    if (code) {
+      decodedMsg.push(alphabetDecode[code]);
+    } else {
+      decodedMsg.push(' ');
+    }
+  }
+
+  return decodedMsg.join('');
 }
 
 function caesarEncode(input, shift) {
-  // Do your stuff here
+  var result = [];
+  var i;
+  var index;
+  var newIndex;
 
-  return "Your result";
+  input = input.toUpperCase();
+
+  for (i = 0; i < input.length; i++) {
+    index = alphabets.indexOf(input.charAt(i));
+
+    if (index === -1) {
+      result.push(input.charAt(i));
+      continue;
+    }
+
+    newIndex = (index + parseInt(shift) + input.length) % input.length;
+    result.push(alphabets[newIndex]);
+  }
+  return result.join('');
 }
 
 // uncomment the relevant line
 // run node script.js ARG_ONE ARG_TWO to test your functions
-// morseDecode(process.argv[2])
-// caesarEncode(process.argv[2], process.argv[3])
+// console.log(morseDecode(process.argv[2]));
+console.log(caesarEncode(process.argv[2], process.argv[3]));
